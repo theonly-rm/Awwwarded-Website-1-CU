f6ede8fc4e2a30df6b28f02c32b0868de74b69fc
Rachit Mahajan <141140128+theonly-rm@users.noreply.github.com>
2023-12-30 00:59:44 +0530

    circleMouseFollower added

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function (details){
+        document.querySelector(".minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`
+    })
+}
+
+circleMouseFollower();
