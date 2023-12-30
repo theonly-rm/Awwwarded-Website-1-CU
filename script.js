c42b295662c8eac2b5d6ad235102ebbe2fb99164
Rachit Mahajan <141140128+theonly-rm@users.noreply.github.com>
2023-12-30 13:09:23 +0530

    hero animations done

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from(".nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem", {
+        y: '0',
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -2,
+        stagger: .2
+    })
+    .from(".herofooter", {
+        y: '-10',
+        opacity: 0,
+        duration: 2,
+        delay: -1.5,
+        ease: Expo.easeInOut
+    })
+}
+
 function circleMouseFollower(){
     window.addEventListener("mousemove", function (details){
         document.querySelector(".minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`
@@ -10,3 +35,6 @@ function circleMouseFollower(){
 }
 
 circleMouseFollower();
+firstPageAnim();
+
+
